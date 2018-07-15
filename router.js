1acdd4cd78619b0614fd9120e06bc50571712fdf
Ross William Cournoyer <rcoupvc@yahoo.com>
2018-07-15 22:16:25 +0800

    Can post to person table

--- router.js
+++ router.js
@@ -1,10 +1,35 @@
 const pool = require('./db')
 
 module.exports = function(app) {
+
     app.get('/', (req, res) => {res.status(200).send('Hello There!')});
+
     app.get('/people', (req, res) => { 
         pool.query('SELECT * FROM person', (err, result) => {
             if (err) return console.log(err);
+            res.status(200).send(result.rows);
+        });
+    });
+
+    app.post('/addperson', (req, res) => {
+        const query = {
+            name: 'add-person',
+            text: 'INSERT INTO person\
+            (first_name, last_name, dob, street_address, city, state, phone, email)\
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+            values: [
+                req.body.firstName, 
+                req.body.lastName, 
+                req.body.dob, 
+                req.body.streetAddress, 
+                req.body.city, 
+                req.body.state, 
+                req.body.phone, 
+                req.body.email
+            ]
+        };
+        pool.query(query, (err, result) => {
+            if (err) console.log(err);
             res.status(200).send(result);
         });
     });
