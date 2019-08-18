1829fb1474526cc56933c1b234b2fd2e40d24b9c
Ross Cournoyer <rcoupvc@yahoo.com>
2019-08-18 17:22:58 +0800

    Try different cors config

--- src/server.js
+++ src/server.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
-app.disable('etag');
-app.use(cors());
+// app.disable('etag');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*', limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
