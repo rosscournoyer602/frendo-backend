780db17cc18883292ee60a75067b682c27b74735
Ross Cournoyer <rcoupvc@yahoo.com>
2019-09-01 15:53:24 +0800

    Change cors settings

--- src/server.js
+++ src/server.js
@@ -8,21 +8,7 @@ const router = require('./router');
 const app = express();
 
 app.disable('etag');
-app.use(
-  cors({
-    origin: 'https://friendo.herokuapp.com'
-  })
-);
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
+app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*', limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
