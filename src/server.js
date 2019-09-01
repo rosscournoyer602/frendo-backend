3fa504d1c3314d3f020b23858e0684c69cff4556
Ross Cournoyer <rcoupvc@yahoo.com>
2019-09-01 15:06:34 +0800

    Change cors settings

--- src/server.js
+++ src/server.js
@@ -8,8 +8,7 @@ const router = require('./router');
 const app = express();
 
 app.disable('etag');
-app.use(cors());
-app.options('*', cors());
+app.use('*', cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*', limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
