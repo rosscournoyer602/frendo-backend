54b80380652b6c15a778e11f78137b01b5535df5
Ross Cournoyer <rcoupvc@yahoo.com>
2019-09-01 13:48:58 +0800

    Change cors settings

--- src/server.js
+++ src/server.js
@@ -8,11 +8,6 @@ const router = require('./router');
 const app = express();
 
 app.disable('etag');
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*', limit: '50mb' }));
