966763863933353a9da8616561c7f4ee13309b12
Ross Cournoyer <rcoupvc@yahoo.com>
2019-09-01 16:21:39 +0800

    Change cors settings

--- src/server.js
+++ src/server.js
@@ -2,22 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 app.disable('etag');
+app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*', limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
 router(app);
 
 app.listen(process.env.PORT || 8080);
