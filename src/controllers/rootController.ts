e199f1d63f3c0e07bb38e30666afab14ba462dc4
Ross Cournoyer <rcoupvc@yahoo.com>
2020-04-13 15:01:09 +0800

    Remove unused files

--- src/controllers/rootController.ts
+++ src/controllers/rootController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { get, controller, use } from './decorators';
-// import { requireAuth } from '../middleware/requireAuth';
 
 @controller('')
 class RootController {
